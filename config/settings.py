342cd7a818d4e2b2fc29796a5053ac7dc66d0801
Satyam Kumar Ray <satyamdev1415@gmail.com>
2026-09-23 13:39:30 +0530

    Allow Render host

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,11 @@ load_dotenv()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    ".onrender.com",
+]
 
 
 # Application definition
